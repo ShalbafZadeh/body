c473f24508e19c5f42e178df385983054e41ae54
shalbafzadeh <rezashalbaf@gmail.com>
2017-01-15 00:27:40 +0330

    Add body.py

---
+++ body.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self,head,tounge):
+        self.head = head
+        self.tounge = tounge
+
+    def behading(self):
+        if self.head == "green" and self.tounge == "red":
+            print ("Person has behaded")
+	else :
+            print ("Person remains alive")
+
+
+person = Person(head="green",tounge="red")
+person.behading()
