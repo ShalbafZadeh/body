302f9863bdcad04121ee1f04e7fdd0d22f32a7da
shalbafzadeh <rezashalbaf@gmail.com>
2017-01-15 00:45:13 +0330

    Make body.py PEP8 friendly

--- body.py
+++ body.py
@@ -1,14 +1,15 @@
 class Person:
-    def __init__(self,head,tounge):
+
+    def __init__(self, head, tounge):
         self.head = head
         self.tounge = tounge
 
     def behading(self):
         if self.head == "green" and self.tounge == "red":
             print ("Person has behaded")
-	else :
+        else:
             print ("Person remains alive")
 
 
-person = Person(head="green",tounge="red")
+person = Person(head="green", tounge="red")
 person.behading()
